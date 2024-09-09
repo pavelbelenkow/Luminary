4379a18a9ec0fb90bf9c08f3cb801a23903be24b
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-09 14:54:16 +0300

    Initial Commit

---
+++ Luminary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Luminary
+//
+//  Created by Pavel Belenkow on 09.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
