9d11a5a2398da2fc35cdc92eeb12e2bfe9300996
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-12 22:03:44 +0300

    Added Const enumeration to store app constants

---
+++ Luminary/Helpers/Const.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+// MARK: - Constants
+
+enum Const {
+    
+    static let firstPage = 1
+    static let limitThirty = 30
+    
+    static let spacingSmall: CGFloat = 8
+    static let spacingMedium: CGFloat = 16
+    static let collectionCellCornerRadius: CGFloat = 15
+    
+    static let repeatButtonBorderWidth: CGFloat = 1
+    static let repeatButtonCornerRadius: CGFloat = 10
+    
+    static let sortIcon = "arrow.up.arrow.down.circle"
+    static let twoColumnsGridIcon = "square.grid.2x2"
+    static let oneColumnGridIcon = "rectangle.grid.1x2"
+    static let searchBarPlaceholder = "Search for inspiration..."
+    static let imagePlaceholder = "photo"
+    static let locationsKeyPath = "locations"
+    static let shimmerAnimationKey = "shimmerAnimation"
+    static let photoCollectionViewCellReuseIdentifier = "photoCell"
+    static let photoCollectionViewFooterReuseIdentifier = "photoCellFooterView"
+    static let searchHistoryCellReuseIdentifier = "searchHistoryCell"
+    
+    static let recentSearchesStorageKey = "recentSearches"
+    static let maxRecentSearches = 5
+}
